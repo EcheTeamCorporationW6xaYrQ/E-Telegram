792e5c19ff1ac3dc5a5cc4d66b7fcd531515b96a
n1llshul <71400266+n1llshul@users.noreply.github.com>
2023-06-06 17:58:31 +0300

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,5 @@
+import telebot
+from telebot import types
+from init.py import bot
+
+#if update.message.chat_id == {}:           #наш чат айді, треба змінити!! необхідне для керування в цілях безпеки
