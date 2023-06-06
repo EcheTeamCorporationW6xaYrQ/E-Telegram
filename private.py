8d58c0469baeb6069c0aca03771f95e6b0a89b43
n1llshul <71400266+n1llshul@users.noreply.github.com>
2023-06-06 17:04:20 +0300

    Update private.py

--- private.py
+++ private.py
@@ -4,4 +4,7 @@ from init.py import bot
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-  
+  if update.message.chat_id == {}:           #наш чат айді, треба змінити!! необхідне для керування в цілях безпеки
+     if message.text == "/status": 
+        #має виводити статус сайту
+        
